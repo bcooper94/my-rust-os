d93f3e2420b5a1acfc9ca7aa346066831a4873ec
Brandon <bcoopercs@live.com>
2020-07-19 15:54:01 -0700

    Create a minimal x86_64 kernel with basic VGA buffer

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+extern crate rlibc;
+
+mod vga_buffer;
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("Hello world{}", "!");
+
+    loop {}
+}
